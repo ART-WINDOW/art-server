6ccb81e63f9a9d32e97be8464b9255ea9d456538
Doma <jang6129@naver.com>
2024-10-24 16:39:33 +0900

    adc update

--- src/main/java/com/doma/artserver/util/storage/GCPstorageService.java
+++ src/main/java/com/doma/artserver/util/storage/GCPstorageService.java
@@ -4,8 +4,6 @@ import com.google.cloud.storage.*;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Value;
 
-import java.io.InputStream;
-
 @Component
 public class GCPstorageService implements StorageService<byte[]> {
 
@@ -23,26 +21,33 @@ public class GCPstorageService implements StorageService<byte[]> {
         BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
 
         Storage.BlobTargetOption precondition;
-        if (storage.get(blobId) != null) {
+        Blob existingBlob = storage.get(blobId); // 기존 Blob 객체 가져오기
+
+        if (existingBlob == null) {
+            // 객체가 존재하지 않는 경우 (새로 업로드할 때)
             precondition = Storage.BlobTargetOption.doesNotExist();
         } else {
-            precondition = Storage.BlobTargetOption.generationMatch(
-                    storage.get(bucketName, fileName).getGeneration());
+            // 객체가 존재하는 경우, generationMatch를 사용하여 업로드 조건 설정
+            precondition = Storage.BlobTargetOption.generationMatch(existingBlob.getGeneration());
         }
 
+        // 파일 업로드
         storage.create(blobInfo, data, precondition);
+        // 모든 사용자에게 읽기 권한 부여
         storage.createAcl(blobId, Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER));
 
+        // 업로드한 파일의 미디어 링크 반환
         return storage.get(blobId).getMediaLink();
     }
 
     @Override
     public void deleteFile(String fileName) {
-
+        // 삭제 로직 구현
     }
 
     @Override
     public byte[] downloadFile(String fileName) {
+        // 다운로드 로직 구현
         return null;
     }
 }
