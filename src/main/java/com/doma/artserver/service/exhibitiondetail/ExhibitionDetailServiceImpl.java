332dd442535a952ad7a9a4a769f05842cff99cc2
Doma <jang6129@naver.com>
2024-11-26 19:24:14 +0900

    update

--- src/main/java/com/doma/artserver/service/exhibitiondetail/ExhibitionDetailServiceImpl.java
+++ src/main/java/com/doma/artserver/service/exhibitiondetail/ExhibitionDetailServiceImpl.java
@@ -26,7 +26,6 @@ public class ExhibitionDetailServiceImpl {
         System.out.println("전시 상세 데이터 수집 시작");
         List<Exhibition> exhibitions = exhibitionRepository.findAll();
         System.out.println("전시 수 : " + exhibitions.size());
-        System.out.println(exhibitions.get(0).getApiId());
 
         for (Exhibition exhibition : exhibitions) {
             List<MunwhaExhibitionDetailDTO> detailList = apiClient.fetchItems(exhibition.getApiId());
