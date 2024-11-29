1be28caea74c34bb5b0d4dc0f60e5f8bda134aa3
Doma <jang6129@naver.com>
2024-11-29 21:16:03 +0900

    WebConfig allowCredentials(true -> false)

--- src/main/java/com/doma/artserver/config/WebConfig.java
+++ src/main/java/com/doma/artserver/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig {
 //                        .allowedOriginPatterns("https://art-window.com") // 허용할 오리진 설정
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 메서드 설정
                         .allowedHeaders("*") // 허용할 헤더 설정
-                        .allowCredentials(true) // 인증 정보 허용 여부
+                        .allowCredentials(false) // 인증 정보 허용 여부
                 ;
             }
         };
