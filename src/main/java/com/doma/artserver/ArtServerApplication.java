c1d6680d05900915990ebc53b46fc9f0beb3e10e
Doma <jang6129@naver.com>
2025-03-30 16:09:18 +0900

    EnableScheduling update

--- src/main/java/com/doma/artserver/ArtServerApplication.java
+++ src/main/java/com/doma/artserver/ArtServerApplication.java
@@ -2,8 +2,10 @@ package com.doma.artserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ArtServerApplication {
 
     public static void main(String[] args) {
